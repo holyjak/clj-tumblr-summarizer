32821b5d7b7c7b0da8f6112f33ec47d0831e2a7f
Jakub Holy <jakubholy@jakubholy.net>
2022-01-23 11:04:40 +0100

    Formatting improvements: lists etc
    * Wrap <li> in ul/ol
    * Respect \n inside text (as tumblr does)
    * Drop subheading is same as summary used in a subsequent link
    * Drop 'GitHub -' from links

--- src/clj_tumblr_summarizer/output.clj
+++ src/clj_tumblr_summarizer/output.clj
@@ -90,6 +90,9 @@
        (->sparse-formatting-tree text)
        (apply-formatting-tree text)))
 
+(defn rm-gh [text]
+  (some-> text (str/replace-first #"^GitHub - " "")))
+
 (defn render-audio-block [{:keys [summary]}
                           {:keys [artist embed_html embed_url title url]}]
   {:pre [url]}
@@ -109,30 +112,30 @@
   #_
   (when (not= title summary)
     (println "WARN: link: (not= title summary)" title "!=" summary))
-  [:span.link "👓 " [:a {:href (unredirect url)} summary]
+  [:span.link "👓 " [:a {:href (unredirect url)} (rm-gh summary)]
    (when (seq tags)
      (list " [" (str/join ", " tags) "]"))
    (when description
-     (list " - "
-       [:q description] ""))])
+     (list " - " [:q description]))])
 
 (defn apply-text-subtype [formatted-text subtype]
   (case subtype
     "heading1" (list [:strong formatted-text] [:br])
     "heading2" (list [:em formatted-text] [:br])
     "indented" [:div.indented {:style "padding-left: 1em"} formatted-text]
-    "ordered-list-item" [:li formatted-text] ; FIXME: Wrap all these in ol/ul, drop <br> between the blocks
+    "ordered-list-item" [:li formatted-text]
     "quote" [:blocquote formatted-text]
     "unordered-list-item" [:li formatted-text]))
 
-;; TODO: turn \n\n into <br> or set css to respect it? See 107205933254
-;; TODO: skip heading2 texts that == the following link text
 ;; TODO: Tags for link-less posts
 ;; TODO: ? auto-link URLs - see e.g. 662792835392831488
 (defn render-text-block [_ {mytxt :text, :keys [formatting subtype]}]
-  (list [:br] (cond-> mytxt
-                (seq formatting) (format-text-content formatting)
-                subtype          (apply-text-subtype subtype))))
+  (let [rendered (cond-> mytxt
+                   (seq formatting) (format-text-content formatting)
+                   subtype          (apply-text-subtype subtype))]
+    (if (and (vector? rendered) (= :li (first rendered)))
+      rendered
+      [:span {:style "white-space: pre-line;"} rendered])))
 
 (defn render-video-block [{:keys [summary]} 
                           {{:keys [display_text]} :attribution
@@ -151,6 +154,28 @@
     "text" (render-text-block post block)
     "video" (render-video-block post block)))
 
+(defn rm-duplicate-heading [summary
+                            [{tx1 :text, ty1 :type}
+                             {ty2 :type :as link}
+                             & more :as content]]
+  (if (and (= (rm-gh tx1) (rm-gh summary))
+        (= ty1 "text") (= ty2 "link"))
+    (cons link more)
+    content))
+
+(defn wrap-lists [post content]
+  (->> content
+       (partition-by (comp #{"ordered-list-item" "unordered-list-item"} :subtype))
+       (mapcat
+         (fn [coll]
+           (let [res (doall (map #(render-block post %) coll))
+                 subtype (-> coll first :subtype)]
+             (cond
+               (-> subtype #{"ordered-list-item"}) (list [:ol res])
+               (-> subtype #{"unordered-list-item"}) (list [:ul res])
+               :else (interpose [:br] res)))))
+       (doall)))
+
 (defn render-post [{:keys [content id layout summary tags type] :as post}]
   (assert (= type "blocks") (str " - :type " type " not supported yet"))
   (assert (< (count layout) 2) (str " - layout len " (count layout) " > 1 not supported"))
@@ -164,7 +189,9 @@
              {:id id, :layout layout, :bad bad-blocks})))
   (try
     [:p
-     (doall (map #(render-block post %) content))]
+     (doall (->> content
+                 (rm-duplicate-heading (:summary post))
+                 (wrap-lists post)))]
     (catch Exception e
       (throw (ex-info
                (str "Failed to render post #" id ": " (ex-message e))
@@ -184,51 +211,20 @@
    ;; TODO: does lways summary == content[t=link].title ?
      (map #(select-keys % [:content :id :layout :summary :tags :timestamp :type :post_url]))))
 
-  (def meander (->> posts (filter #(= 186840427164 (:id %))) first))
   (def post (->> posts (filter #(= 665732322540322816 (:id %))) first))
-  (clojure.pprint/pprint
-    (->> post :content second :type))
-  (render-post post)
   (doall (map #(render-block post %) (:content post)))
 
-  (def failed
-    (->> posts
-         (mapv
-           #(try (render-post %)
-              nil
-              (catch Throwable e
-                [:FAIL (:id %) (ex-message e)])))
-         (remove nil?)))
-
-  (count posts) ;; => 791
-  (count failed) ;; => 101
-  (->> failed (map #(nth % 2)) set count) ; 5 uniq errs
-  ;; TODO: Content type "video" "image" "audio"
-  (->> failed
-       (map #(nth % 2))
-       (group-by identity)
-       (map (fn [[k v]] [k (count v)]))
-       (sort-by second)
-       reverse
-       (clojure.pprint/pprint))
-  (first failed)
-  (tap> failed)
-  video
-  (keys video)
-
-  (tap> (with-meta [:h3 "test html!"]
-          {:portal.viewer/default :portal.viewer/hiccup}))
-
-  (pr (render-post meander))
-  (tap> (with-meta (render-post meander)
-          {:portal.viewer/default :portal.viewer/hiccup}))
+  (pr (render-post post))
+  (clojure.pprint/pprint (render-post post))
+  (tap> (with-meta (render-post post)
+          {:portal.viewer/default :portal.viewer/hiccup})) ; :portal.viewer/tree
 
   (run! #(try (println (render-post %)) (print \.)
            (catch Exception e
              (println "ERROR Failed to render ID " (:id %) ":" e)
-             (throw e))) 
+             (throw e)))
     posts)
-  
+
   (def id->post-html (-> (slurp "orig-posts.edn") (clojure.edn/read-string)))
 
   (spit "out.html"
@@ -237,25 +233,14 @@
       [:body
        [:table
         (map #(list
-               [:tr [:td {:colspan 2} [:hr] [:a {:href (:post_url %)} "src"]]] 
+                [:tr [:td {:colspan 2} [:hr] [:a {:href (:post_url %)} "src"]]]
                 [:tr
                  [:td {:style "width: 50%"} (render-post %)]
-                 [:td {:style "width: 50%"} (h.util/raw-string (-> % :id id->post-html))]]) 
+                 [:td {:style "width: 50%"} (h.util/raw-string (-> % :id id->post-html))]])
           posts)]]))
-  
 
-  (tap> (with-meta (render-post meander)
-          {:portal.viewer/default :portal.viewer/tree}))
   )
 
 
 #_(def sample (json/read-str
                 (slurp "sample-post.json")))
-
-;; FIXME:
-;; WARN: link: (not= title summary) Plausible Analytics != Plausible Analytics | Simple, privacy-friendly alternative to Google Analytics
-;; WARN: link: (not= title summary) GitHub - you-dont-need/You-Dont-Need-Momentjs: List of functions which you can use to replace moment.js + ESLint Plugin 
-;;                                  != you-dont-need/You-Dont-Need-Momentjs: List of functions which you can use to replace moment.js + ESLint Plugin
- 
-
-
