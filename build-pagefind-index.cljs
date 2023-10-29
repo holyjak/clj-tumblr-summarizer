a8b2ca8a9f777e4a9059fa0f1381ded24e5f1a0f
Jakub Holy <jakubholy@jakubholy.net>
2023-10-30 00:34:00 +0100

    Add offline search with Pagefind.app
    Run `bb index` to build the search index & supporting files,
    then serve `./search.html` + the generated `./pagefind/` to be able to search.
    
    Uses the pagefind Node lib to be able to index .edn files as "custom records".
    Written with Squint, so that I don't need to write JS :-)

---
+++ build-pagefind-index.cljs
@@ -0,0 +1,54 @@
+;; TODO: Add to the index incrementally, only new stuff => faster; currently takes 3 min
+(ns build-pagefind-index
+  "Build a pagefind search index from .edn Tumblr entries previously downloaded
+   into /data, together with the supporting .js, .css and other files.
+   Serve statically ./search.html and ./pagefind/ to use the search."
+  {:clj-kondo/ignore true}
+  (:require
+   ["fs" :as fs]
+   ["node:fs/promises" :refer [readdir readFile]]
+   ["url" :refer [fileURLToPath]]
+   ["pagefind" :as pagefind]
+   ["edn-data" :refer [parseEDNString]]))
+
+(defn entry->record [{:keys [tags summary content timestamp post_url date] :as _entry}]
+  (try
+   (let [date (when date (.substring date 0 10))
+         content (-> (.map content (fn [{:keys [text description]}] 
+                                    ;; type 'link' -> description, text -> text
+                                     (or text description)))
+                     (.join "\n"))]
+     {:url post_url
+      :content content
+      :language "en"
+     ;; optional:
+      :meta (cond-> {:title summary
+                     ;; These below will be displayed below the result
+                     :date date #_image}
+              (seq tags)
+              (assoc! :tags (-> tags (.sort) (.join ", "))))
+      :filters {:tags tags}
+      :sort {:date date}})
+    (catch :default err
+      (println "ERROR processing entry" _entry ":" (.-message err))
+      (throw err))))
+
+(defn ^:async index-all []
+  (let [entries (js-await (readdir "data"))
+        index (.-index (js-await (pagefind/createIndex)))]
+    ;; Add all entries to the index:
+    (->
+     (for [e entries
+           :when (.endsWith e ".edn")]
+       (-> (readFile (str "data/" e) {:encoding "utf8"})
+           (.then #(parseEDNString % {:mapAs "object" :keywordAs "string"}))
+           (.then entry->record)
+           (.then #(.addCustomRecord index %))))
+     (Promise/all)
+     (js-await)) 
+    ;; Save the index:
+    (js-await (.writeFiles index {:outputPath "pagefind"}))
+    (println "Index written with" (count entries) "entries")
+    (js-await (.close pagefind))))
+
+(js-await (index-all))
